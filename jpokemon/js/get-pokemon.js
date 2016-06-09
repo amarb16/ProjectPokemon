59bb728058301eb0a3a6ac4261e5e4fd8fc77bbf
jsimb17 <jsimbartl_2017@depauw.edu>
2016-06-09 16:06:44 -0400

    Add files via upload

--- jpokemon/js/get-pokemon.js
+++ jpokemon/js/get-pokemon.js
@@ -1,12 +1,16 @@
 var url = 'https://pokeapi.co/api/v2/pokemon/?limit=6&offset=20'
 
-var template1 = $('.template1')
+var myTemplate = $('.myTemplate')
   .detach()
-  .removeClass('template1')
+  .removeClass('myTemplate')
 
-  var template2 = $('.template2')
-    .detach()
-    .removeClass('template2')
+var rivalTemplate = $('.rivalTemplate')
+  .detach()
+  .removeClass('rivalTemplate')
+
+//var attackTemplate = $('.attackTemplate')
+//  .detach()
+//  .removeClass('attackTemplate')
 
 function loadPokemon(pokemon) {
   $.each(pokemon.results, function(i, pokemon) {
@@ -15,23 +19,57 @@ function loadPokemon(pokemon) {
 }
 
 function addPokemon(pokemon) {
-  var li = template1.clone();
+  var li = myTemplate.clone();
   li.find('.pokemon-name a')
     .text(pokemon.name)
     .attr('href', pokemon.url)
 
   li.attr('data-id', pokemon.id);
-  $('#pokemonList1').append(li);
+  $('#myPokemonList').append(li);
 
-  var li = template2.clone();
+  var li = rivalTemplate.clone();
   li.find('.pokemon-name a')
     .text(pokemon.name)
     .attr('href', pokemon.url)
 
   li.attr('data-id', pokemon.id);
-  $('#pokemonList2').append(li);
+  $('#rivalPokemonList').append(li);
 }
 
+function displayMyPokemon(ev){
+  ev.preventDefault();
+  var url = ev.currentTarget.href;
+  $.get({
+      url: url,
+    }).done(function(item) {
+        $('#myPokemon').attr('src', item.sprites.front_default);
+      });
+}
+
+function displayRivalPokemon(ev){
+  ev.preventDefault();
+  var url = ev.currentTarget.href;
+  $.get({
+      url: url,
+    }).done(function(item) {
+        $('#rivalPokemon').attr('src', item.sprites.front_default);
+
+      });
+}
+
+//function displayMyAttack(ev){
+//  ev.preventDefault();
+//  var url = ev.currentTarget.href;
+//  $.get({
+//      url: url,
+//    }).done(function(item) {
+//        $('#myAttack').attr();
+//    });
+//}
+
+$(document).on('click', 'a.myPokemon', displayMyPokemon);
+$(document).on('click', 'a.rivalPokemon', displayRivalPokemon);
+//$(document).on('click', 'a.myAttack'), displayMyAttack);
 $.get({
   url: url,
   success: loadPokemon
